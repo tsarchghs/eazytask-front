b9e942d635c0be19e54bfc9ded6d2bff0343600a
Gjergj Kadriu <gjergjk71@gmail.com>
2020-06-01 21:25:30 +0200

    Switched to HTTPS

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // let baseURL = "http://localhost:4000/api/v1";       
 // let baseURL = "https://app.swaggerhub.com/apis/gjergjk71/easytask/1.0.0-oas3";
-let baseURL = "http://eazytask-back.herokuapp.com/api/v1"
+let baseURL = "https://eazytask-back.herokuapp.com/api/v1"
 
 const instance = axios.create({
     baseURL: baseURL
