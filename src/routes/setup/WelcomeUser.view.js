2b38d23ba21d597bd718175bed6aeaae50d2908c
Gjergj Kadriu <gjergjk71@gmail.com>
2020-06-02 21:32:53 +0200

    Added image

--- src/routes/setup/WelcomeUser.view.js
+++ src/routes/setup/WelcomeUser.view.js
@@ -8,9 +8,9 @@ export default props => {
                 <h1>Account Setup</h1>
                 <p className="shadow__title">setup your account</p>
             </div>
-            <img className="img__mobile" src="http://localhost:3000/images/Group.png" alt="" />
+            <img className="img__mobile" src="/images/Group.png" alt="" />
             <h3>Welcome {props.first_name}!</h3>
             <h5 className="flex-grow">Do you want to <br /> setup your account now?</h5>
-        </React.Fragment>
+            </React.Fragment>
     )
 }
