50e8d0121fff8d3d4403446441d85de5bd649068
Gjergj Kadriu <gjergjk71@gmail.com>
2020-06-17 18:52:33 +0200

    Build

---
+++ build/precache-manifest.57848d72e875bb1fe7f259a6269d974b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb152ba412604d06415ad02999bfb56a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3da376a9e7e8e6af86b5",
+    "url": "/static/css/main.39fc2903.chunk.css"
+  },
+  {
+    "revision": "f49afd90a2777cd4edf1",
+    "url": "/static/js/2.384425fd.chunk.js"
+  },
+  {
+    "revision": "6fdfc0004f105b555cea083a8be220e5",
+    "url": "/static/js/2.384425fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3da376a9e7e8e6af86b5",
+    "url": "/static/js/main.26f89b2c.chunk.js"
+  },
+  {
+    "revision": "6dbafc05dd3f7c318f5a",
+    "url": "/static/js/runtime-main.5e2e2b12.js"
+  },
+  {
+    "revision": "dba0c688b8d5ee09a1e214aebd5d25e4",
+    "url": "/static/media/product-sans-bold.dba0c688.ttf"
+  },
+  {
+    "revision": "eae9c18cee82a8a1a52e654911f8fe83",
+    "url": "/static/media/product-sans.eae9c18c.ttf"
+  },
+  {
+    "revision": "b2fc280467cf7dfbeb91469f24a9475b",
+    "url": "/static/media/register-img.b2fc2804.jpeg"
+  },
+  {
+    "revision": "cde37b286429d3ef5850b3d3a711eb5b",
+    "url": "/static/media/ustah.cde37b28.jpeg"
+  }
+]);
