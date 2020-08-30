381734cf607991f7189293e2a534fd7a63577bcc
Gjergj Kadriu <gjergjk71@gmail.com>
2020-08-31 01:59:28 +0200

    Translations

--- src/routes/setup/BecomeTasker.view.js
+++ src/routes/setup/BecomeTasker.view.js
@@ -17,9 +17,9 @@ export default props => {
                 {props.getTrans(props.translations.text_25)}
             </h5>
             <p className="special flex-grow fs18">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. <br/>
-                Labore accusantium delectus autem voluptate at odit 
-            </p>
+            {props.translations.text_48[props.app_lang].map(
+                i => <React.Fragment>{i} <br/></React.Fragment>
+            )}            </p>
             <img className="img__mobile" src="/images/checklist.png" alt="" />
         </React.Fragment>
     )
