30198c32aaf07032bd53f64b878f58cc83993b56
Gjergj Kadriu <gjergjk71@gmail.com>
2020-09-30 01:30:07 +0200

    Changed api endpoint

--- src/configs.js
+++ src/configs.js
@@ -1,13 +1,13 @@
 // export const baseURL = "http://localhost:4000/api/v1";
-// export const baseURL_WS = "https://eazytask-back.herokuapp.com/"
-export const baseURL = "https://eazytask-back-staging.herokuapp.com/api/v1"
+export const baseURL_WS = "https://eazytask-back.herokuapp.com/"
+// export const baseURL = "https://eazytask-back-staging.herokuapp.com/api/v1"
 // export const baseURL = "https://app.swaggerhub.com/apis/gjergjk71/easytask/1.0.0-oas3";
 // export const baseURL = "http://192.168.0.109:4000/api/v1"
 // export const baseURL = "http://192.168.0.31:4000/api/v1"
 
 // export const baseURL_WS = "http://localhost:4000/";
-// export const baseURL_WS = "https://eazytask-back.herokuapp.com/"
-export const baseURL_WS = "https://eazytask-back-staging.herokuapp.com/"
+export const baseURL_WS = "https://eazytask-back.herokuapp.com/"
+// export const baseURL_WS = "https://eazytask-back-staging.herokuapp.com/"
 // export const baseURL_WS = "https://app.swaggerhub.com/apis/gjergjk71/easytask/1.0.0-oas3";
 // export const baseURL_WS = "http://192.168.0.109:4000/"
 // export const baseURL_WS = "http://192.168.0.31:4000/"
